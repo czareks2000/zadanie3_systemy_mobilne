5e4690ab14c52af358aed1cae5a2fc5f3ad0638c
Cezary Stachurski <czareks20006@gmail.com>
2023-10-24 17:19:13 +0200

    first commit

---
+++ app/src/main/java/pl/edu/pb/todoapp/TaskStorage.java
@@ -0,0 +1,41 @@
+package pl.edu.pb.todoapp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class TaskStorage {
+    private static TaskStorage instance = new TaskStorage();
+    private List<Task> tasks;
+
+    private TaskStorage() {
+        final int tasksCount = 20;
+        tasks = new ArrayList<Task>(tasksCount);
+        for (int i = 1; i <= tasksCount; ++i) {
+            Task task = new Task();
+            task.setName("Task" + i);
+            task.setDone(i % 3 == 0);
+            tasks.add(task);
+        }
+    }
+
+    public static TaskStorage getInstance() {
+        return instance;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public Task getTask(UUID id) {
+        for (Task task : tasks) {
+            if (task.getId().equals(id))
+                return task;
+        }
+        return null;
+    }
+}
